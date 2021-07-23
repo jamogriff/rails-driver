d45904075bd5abc92c8f1231be12b1d067e4d791
Jamison Griffith <jamison.griffith@gmail.com>
2021-07-23 14:10:18 -0600

    Implement JS for weekly revenue on dashboard

--- app/controllers/admin/merchants/search_controller.rb
+++ app/controllers/admin/merchants/search_controller.rb
@@ -1,7 +1,7 @@
 class Admin::Merchants::SearchController < ApplicationController
   def index
     @search_term = params[:merchant_name]
-    response = Faraday.get("#{ENV['RAILS_ENGINE_DOMAIN']}/api/v1/merchants/find_all?name=#{@search_term}")
+    response = Faraday.get("#{ENV['RAILS_ENGINE_DOMAIN']}/api/v1/merchants/find?name=#{@search_term}")
     merchants_data = JSON.parse(response.body, symbolize_names: true)
     @merchants = merchants_data[:data].map do |merchant_data|
       Merchant.new(merchant_data)
